936dc6b714200c6fbd0408fc2344f842dae7ff89
dibu2018 <erieljr1@gmail.com>
2018-07-20 23:12:02 -0400

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static/droper'
   get 'static/droper'
   post 'static/droper' => 'static#upload'
 
